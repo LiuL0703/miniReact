c8412cec94b7a458e8dc48c6443d193037e8c723
liul0703 <louieliu0703@gmail.com>
2020-07-05 18:32:58 +0800

    feat:update&delete

---
+++ src/index_v1.js
@@ -0,0 +1,50 @@
+function createElement(type, props, ...children){
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === 'object' ? child : createTextElement(child))
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+function render(element, container){
+  const dom = element.type === 'TEXT_ELEMENT' 
+    ? document.createTextNode("") 
+    : document.createElement(element.type)
+
+  const isProperty = key => key !== "children";
+  Object.keys(element.props).filter(isProperty).forEach(name => {
+    dom[name] = element.props[name]
+  }) 
+
+  element.props.children.forEach(child => render(child, dom))
+  container.appendChild(dom)
+}
+
+const MiniReact = {
+  createElement,
+  render
+}
+// babel转译时使用自定义的createElement 而不是React默认的
+/** @jsx MiniReact.createElement */
+const element = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from MiniReact</h2>
+  </div>
+);
+
+const container = document.getElementById('root')
+MiniReact.render(element, container);
+
--- src/index_v2.js
+++ src/index_v2.js
@@ -195,39 +195,13 @@ function reconcileChildren(wipFiber, elements){
   }
 }
 
-// function render(element, container){
-//   const dom = element.type === 'TEXT_ELEMENT' 
-//     ? document.createTextNode("") 
-//     : document.createElement(element.type)
-
-//   const isProperty = key => key !== "children";
-//   Object.keys(element.props).filter(isProperty).forEach(name => {
-//     dom[name] = element.props[name]
-//   }) 
-
-//   // 一旦递归开始 无法暂停去执行高优先级任务比如响应用户输入等
-//   element.props.children.forEach(child => render(child, dom))
-//   container.appendChild(dom)
-// }
-
 const MiniReact = {
   createElement,
   render
 }
 // babel转译时使用自定义的createElement 而不是React默认的
 /** @jsx MiniReact.createElement */
-// const element = (
-//   <div style="background: salmon">
-//     <h1>Hello World</h1>
-//     <h2 style="text-align:right">from MiniReact</h2>
-//   </div>
-// );
-
 const container = document.getElementById('root')
-// MiniReact.render(element, container);
-
-
-
 const updateValue = e => {
   rerender(e.target.value)
 }
