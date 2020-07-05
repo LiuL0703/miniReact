8c339ea6ce399e2e3190610e64cee59e5e97439f
liul0703 <louieliu0703@gmail.com>
2020-07-05 15:24:48 +0800

    feat:custom miniReact

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+function createElement(type, props, ...children){
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === 'object' ? child : createTextElement(child))
+    }
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+function render(element, container){
+  const dom = element.type === 'TEXT_ELEMENT' 
+    ? document.createTextNode("") 
+    : document.createElement(element.type)
+
+  const isProperty = key => key !== "children";
+  Object.keys(element.props).filter(isProperty).forEach(name => {
+    dom[name] = element.props[name]
+  }) 
+
+  element.props.children.forEach(child => render(child, dom))
+  container.appendChild(dom)
+}
+
+const MiniReact = {
+  createElement,
+  render
+}
+// babel转译时使用自定义的createElement 而不是React默认的
+/** @jsx MiniReact.createElement */
+const element = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from MiniReact</h2>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById('root')
+MiniReact.render(element, container);
+
+
+
+
+
+
